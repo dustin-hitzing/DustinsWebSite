7be73bffbbaccce25ceb568655c813a53487eeff
dustin.hitzing <dustinhitzing@gmail.com>
2016-03-07 11:34:33 -0500

    Made model, set DbContext, updated database

---
+++ DustinPortfolio/Domain/Work.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DustinPortfolio.Domain {
+    public class Work {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+
+    }
+}
---
+++ DustinPortfolio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DustinPortfolio.WebApiApplication" Language="C#" %>
